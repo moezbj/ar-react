31bc092c435d71e5e08138b1cb73daec75491181
moez <moezbj.93@gmail.com>
2024-09-18 22:27:24 +0100

    change the ring

--- src/components/xr-hit-model/XrHitModel.jsx
+++ src/components/xr-hit-model/XrHitModel.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+/* import { OrbitControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { Interactive, useHitTest, useXR } from "@react-three/xr";
 import { useRef, useState } from "react";
@@ -45,6 +45,67 @@ const XrHitModel = () => {
           <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
             <ringGeometry args={[0.1, 0.25, 32]} />
             <meshStandardMaterial color={"white"} />
+          </mesh>
+        </Interactive>
+      )}
+
+      {!isPresenting && <Model />}
+    </>
+  );
+};
+
+export default XrHitModel;
+ */
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Interactive, useHitTest, useXR } from "@react-three/xr";
+import { useRef, useState } from "react";
+import * as THREE from "three";
+import Model from "./Model";
+
+const XrHitModel = () => {
+  const reticleRef = useRef();
+  const [models, setModels] = useState([]);
+  const { isPresenting } = useXR();
+
+  // Load the image texture for the hit marker
+  const hitMarkerTexture = useTexture("/models/hand.png");
+
+  useThree(({ camera }) => {
+    if (!isPresenting) {
+      camera.position.z = 3;
+    }
+  });
+
+  useHitTest((hitMatrix, hit) => {
+    hitMatrix.decompose(
+      reticleRef.current.position,
+      reticleRef.current.quaternion,
+      reticleRef.current.scale
+    );
+    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
+  });
+
+  const placeModel = (e) => {
+    let position = e.intersection.object.position.clone();
+    let id = Date.now();
+    setModels((prevModels) => [...prevModels, { position, id }]);
+  };
+
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight />
+      {isPresenting &&
+        models.map(({ position, id }) => {
+          return <Model key={id} position={position} />;
+        })}
+      {isPresenting && (
+        <Interactive onSelect={placeModel}>
+          <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
+            {/* Replace ring geometry with a plane and apply the image texture */}
+            <planeGeometry args={[0.3, 0.3]} />
+            <meshBasicMaterial map={hitMarkerTexture} transparent />
           </mesh>
         </Interactive>
       )}
