85e382fd67f9c7f945c0737f2028642d9b4157d1
moez <moezbj.93@gmail.com>
2024-09-18 18:35:48 +0100

    back to 1

--- src/components/xr-hit-model/Model.jsx
+++ src/components/xr-hit-model/Model.jsx
@@ -1,35 +1,25 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
 
 export default function Model(props) {
   const group = useRef();
-  const texture = useLoader(THREE.TextureLoader, "/henna.png");
-
-  useEffect(() => {
-    if (texture) {
-      const aspectRatio = texture.image.width / texture.image.height;
-      const planeWidth = 1; // Set this to the desired width
-      const planeHeight = planeWidth / aspectRatio;
-      // Adjust the plane geometry to the image aspect ratio
-      imageRef.current.geometry.dispose(); // Dispose the old geometry
-      imageRef.current.geometry = new THREE.PlaneGeometry(
-        planeWidth,
-        planeHeight
-      );
-    }
-  }, [texture]);
-  // const { nodes, materials, animations } = useGLTF("/models/druid.gltf");
+  const { nodes, materials, animations } = useGLTF("/models/druid.gltf");
   const { actions } = useAnimations(animations, group);
   return (
-    <mesh ref={imageRef}>
-      <planeGeometry args={[1, 1]} />
-      <meshBasicMaterial map={texture} />
-    </mesh>
+    <group ref={group} {...props} dispose={null}>
+      <group scale={1.91}>
+        <primitive object={nodes.root} />
+        <skinnedMesh
+          geometry={nodes.druid.geometry}
+          material={materials.color_main}
+          skeleton={nodes.druid.skeleton}
+        />
+      </group>
+    </group>
   );
 }
 
-// useGLTF.preload("/models/druid.gltf");
+useGLTF.preload("/models/druid.gltf");
 
 // import { useLoader } from "@react-three/fiber";
 // import { Suspense } from "react";
