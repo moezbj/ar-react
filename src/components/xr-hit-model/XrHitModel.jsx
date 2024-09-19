bdef216f186c1658a938f9fe1b68f6fe150a4c31
moez <moezbj.93@gmail.com>
2024-09-19 03:14:12 +0100

    test 2.4

--- src/components/xr-hit-model/XrHitModel.jsx
+++ src/components/xr-hit-model/XrHitModel.jsx
@@ -17,13 +17,15 @@ const XrHitModel = () => {
   });
 
   useHitTest((hitMatrix, hit) => {
-    hitMatrix.decompose(
+    /* hitMatrix.decompose(
       reticleRef.current.position,
       reticleRef.current.quaternion,
       reticleRef.current.scale
     );
 
-    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
+    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0); */
+    reticleRef.current.position.clone().add(offset);
+    reticleRef.current.rotation.copy(camera.rotation);
   });
 
   const placeModel = (e) => {
