6fce3374acb99101d212be9b8e76a3cf579ea49b
moez <moezbj.93@gmail.com>
2024-09-19 02:59:43 +0100

    rerturn to hit 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import XrHitModelContainer from "./components/xr-hit-model/XrHitModelContainer";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<XrHitCubeContainer />} />
+      <Route path="/" element={<XrHitModelContainer />} />
       <Route path="/cube" element={<CubeContainer />} />
       <Route path="/xr-cube" element={<XrCubeContainer />} />
       <Route path="/xr-hit-cube" element={<XrHitCubeContainer />} />
